8fb813631f1f560e0cbf0f779c2534c1939281d0
aoqia <aoqia@aoqia.dev>
2026-08-31 16:46:22 +1000

    fix(build): remove deprecated property syntax

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -34,9 +34,7 @@ if (!JavaVersion.current().isCompatibleWith(JavaVersion.VERSION_21)) {
 	throw UnsupportedOperationException("Leaf Loader requires Java 21+ to build.")
 }
 
-// Because Gradle makes it read-only in build scripts
-val name: String by settings
-rootProject.name = name
+rootProject.name = providers.gradleProperty("name").get()
 
 include("junit")
 include("zomboid")
