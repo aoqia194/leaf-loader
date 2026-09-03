68bebadd7df40ecf477f2f8af7df535f0c85d8f4
aoqia <aoqia@aoqia.dev>
2026-09-03 12:12:15 +1000

    fix(chore): remove unused import

--- src/main/java/dev/aoqia/leaf/loader/impl/util/Arguments.java
+++ src/main/java/dev/aoqia/leaf/loader/impl/util/Arguments.java
@@ -21,7 +21,6 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.List;
